672601abd5969a846c38adc59bae1dacdb1e01e2
Albert <avalls89@gmail.com>
2022-01-24 20:04:17 +0100

    24/01/22

---
+++ src/closures/private.js
@@ -0,0 +1,19 @@
+const person = () => {
+    var saveName = "Name";
+    return {
+        getName: () => {
+            return saveName;
+        },
+        setName: (name) => {
+            saveName = name;
+        },
+    };
+};
+
+newPerson =  person();
+
+console.log(newPerson.getName());
+
+newPerson.setName('Albert');
+
+console.log(newPerson.getName());
--- src/scope/block.js
+++ src/scope/block.js
@@ -30,7 +30,7 @@ console.log(x);
 
 
 const anotherFunction = () => {
-    for (var i=0; i<10; i++){
+    for (let i=0; i<10; i++){
         setTimeout(() =>{
             console.log(i);
         }, 1000)
